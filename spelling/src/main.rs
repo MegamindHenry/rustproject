b809f07d7f3bf3f00a3732abe8c8c1be61927807
MegamindHenry <hakunamatatahenry@gmail.com>
2018-08-09 23:39:46 +0200

    dict
    read from a file and import to a dict

--- spelling/src/main.rs
+++ spelling/src/main.rs
@@ -1,112 +1,35 @@
 use std::fs::File;
 use std::io::Result;
 use std::io::prelude::*;
-use std::cmp::Ordering;
+// use std::cmp::Ordering;
+use std::collections::HashMap;
 
-#[derive(Eq)]
-struct DictWord {
-    pub content: String,
-    pub count: u32,
-}
-
-impl PartialOrd for DictWord {
-    fn partial_cmp(&self, other: &DictWord) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for DictWord {
-    fn cmp(&self, other: &DictWord) -> Ordering {
-        self.content.cmp(&other.content)
-    }
-}
-
-impl PartialEq for DictWord {
-    fn eq(&self, other: &DictWord) -> bool {
-        self.content == other.content
-    }
-}
 
 fn main() -> Result<()> {
-    let mut contents = String::new();
-    contents = read_file("foo.txt").unwrap();
 
-    let dict = form_dict(contents);
+	let filename = "foo1.txt";
 
-    // println!("{:?}", dict[11].content);
+	let dict = dicts(filename).unwrap();
+
+	// println!("{:?}", dict);
 
     Ok(())
 }
 
-//read the file and return a String
-fn read_file(filename: &str) -> Result<String> {
-    let mut file = File::open(filename)?;
+fn dicts(filename: &str) -> Result<HashMap<String, u32>> {
 
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+	let mut file = File::open(filename)?; 
 
-    Ok(contents)
-}
+    let mut text = String::new();
+    file.read_to_string(&mut text)?;
 
-//create a dictionary with a String
-fn form_dict(contents: String) -> Vec<DictWord> {
-    let contents = contents.to_lowercase();
+	let mut map = HashMap::new();
 
-    let iter = contents.split_whitespace();
-
-    let mut dict: Vec<DictWord> = Vec::new();
-    let mut count = 0;
-
-    for content in iter {
-
-    	// let find = dict.binary_search(&content);
-
-    	count += 1;
-    	println!("{:?}", count);
-    	// println!("{:?}", content);
-
-    	let mut find = find(&dict, &content);
-    	// let find = -1;
-
-    	// println!("{:?}", find);
-    	// if find {println!("{:?}", &content)};
-
-    	if find == -1 {
-	        let word = DictWord {
-	            content: content.to_string(),
-	            count: 1u32,
-	        };
-	        dict.push(word);
-	    }else {
-	    	let location: usize = find as usize;
-	    	// println!("{:?}", location);
-	    	dict[location].count+=1;
-
-	    	// let x: () = location;
-	    }
-    }
-    // println!("{:?}", dict.len());
-
-    // for x in dict.iter() {
-    // 	if x.count > 1 {
-	   //  	println!("{:?}   {:?}", x.content, x.count);
-	   //  }
-    // }
-
-    // println!("{:?}", 1111);
-
-    dict
-}
-
-fn find(ref dict: &Vec<DictWord>, target: &str) -> i32 {
-	let mut location = 0;
-
-	for x in dict.iter() {
-		if x.content == target {
-			return location;
-		}
-		location+=1;
+	for word in text.split_whitespace() {
+		let word = word.to_string();
+	    let count = map.entry(word).or_insert(0);
+	    *count += 1;
 	}
 
-	-1
+	Ok(map)
 }
